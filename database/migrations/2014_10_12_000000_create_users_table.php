a365f230619296780feb7227d510b302b6e14efa
k1adi <rizkiadinugroho3@gmail.com>
2024-05-28 17:23:18 +0700

    create migrations for master data table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username', 100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone', 16);
+            $table->string('nik', 16);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2024_05_28_023555_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->enum('type', ['offline', 'online']);
+            $table->string('trainer', 100);
+            $table->string('thumbnail')->nullable();
+            $table->text('url_attachment');
+            $table->string('prerequisite')->nullable();
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
