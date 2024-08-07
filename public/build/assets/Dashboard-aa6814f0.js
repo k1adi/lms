be329e9bac08aae3cdd4d49c128b2494859e69ec
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-08-07 16:49:37 +0700

    try to update

---
+++ public/build/assets/Dashboard-aa6814f0.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-fe8dc069.js";import{D as o}from"./DashboardLayout-aceb6948.js";import"./graduation-cap-2609af60.js";function r(){return a.jsx("div",{className:"content-box",children:a.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=t=>a.jsx(o,{title:"Dashboard",children:t});export{r as default};
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -7,8 +7,9 @@ export default function Guest({ children }) {
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-primary-lighter dark:bg-sky-400">
             <div>
                 <Link href="/">
-                <GraduationCap className='text-center mx-auto text-primary dark:text-white w-28 h-28 sm:w-36 sm:h-36' />
+                    <GraduationCap className='text-center mx-auto text-primary dark:text-white w-28 h-28 sm:w-36 sm:h-36' />
                 </Link>
+                <p>test</p>
             </div>
 
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
