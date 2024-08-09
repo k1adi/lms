ea090747c5764747d517ac81902876cfd0d088d8
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-08-09 10:55:28 +0700

    build production

---
+++ public/build/assets/Dashboard-8b0c6ad9.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-986895ee.js";import{D as o}from"./DashboardLayout-34fd0cff.js";import"./graduation-cap-88a466b4.js";function r(){return a.jsx("div",{className:"content-box",children:a.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=t=>a.jsx(o,{title:"Dashboard",children:t});export{r as default};
--- public/build/assets/Dashboard-fd3db5d8.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./app-70c76b70.js";import{D as o}from"./DashboardLayout-7352f75d.js";import"./graduation-cap-6d67c87b.js";function r(){return a.jsx("div",{className:"content-box",children:a.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=t=>a.jsx(o,{title:"Dashboard",children:t});export{r as default};
