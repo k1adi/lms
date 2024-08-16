96e7882481e09a8b5516e7feecb1f5f81b17ea40
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-08-16 11:16:39 +0700

    build production

---
+++ public/build/assets/Dashboard-71118314.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-6d2a5fe4.js";import{D as o}from"./DashboardLayout-8e4bec36.js";import"./graduation-cap-46ebf896.js";function r(){return a.jsx("div",{className:"content-box",children:a.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=t=>a.jsx(o,{title:"Dashboard",children:t});export{r as default};
