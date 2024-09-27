83d6f7198d83217bd7d0c09b119f07ebcd355b6a
Rizki Adi Nugroho <rizkiadinugroho3@gmail.com>
2024-09-27 11:29:06 +0700

    build production

---
+++ public/build/assets/Dashboard-501fe08f.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-6affcc5d.js";import{D as o}from"./DashboardLayout-785beda9.js";import"./graduation-cap-b3db8b75.js";function r(){return a.jsx("div",{className:"content-box",children:a.jsx("h1",{className:"text--title",children:"Dashboard"})})}r.layout=t=>a.jsx(o,{title:"Dashboard",children:t});export{r as default};
--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import Checkbox from '@/Components/Checkbox';
 import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
 import FieldGroup from '@/Components/Form/FieldGroup';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
@@ -23,7 +21,6 @@ export default function Login({ status, canResetPassword }) {
 
     const submit = (e) => {
         e.preventDefault();
-
         post(route('login'));
     };
 
@@ -72,39 +69,7 @@ export default function Login({ status, canResetPassword }) {
                             placeholder="Password..."
                         />
                     </FieldGroup>
-                    {/* <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        required
-                        onChange={(e) => setData('email', e.target.value)}
-                    />
-
-                    <InputError message={errors.email} className="mt-2" /> */}
                 </div>
-
-                {/* <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        required
-                        onChange={(e) => setData('password', e.target.value)}
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div> */}
 
                 <div className="block mt-4">
                     <label className="flex items-center">
@@ -118,14 +83,14 @@ export default function Login({ status, canResetPassword }) {
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
-                    {/* {canResetPassword && (
+                    {canResetPassword && (
                         <Link
                             href={route('password.request')}
                             className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                         >
                             Forgot your password?
                         </Link>
-                    )} */}
+                    )}
 
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
